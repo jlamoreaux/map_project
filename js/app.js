fa7c47803b2bc3269845882575cec5c4afc5137b
Jorcan Lamoreaux <jnacious88@gmail.com>
2017-10-19 18:48:49 -0500

    locations updated

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ function AppViewModel() {
 
     // Creates a new map centered on Austin, TX
     map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 30.3074624, lng: -98.0335911},
+      center: {lat: 30.267153, lng: -97.7430608},
       zoom: 13,
     });
 
@@ -20,16 +20,15 @@ function AppViewModel() {
 };
 
 AppViewModel();
-/*function Model() {
+
+function Model() {
 
   // Locations that will be marked on the map
   var locations = [
-    {title: '', location: {lat: , lng: }},
-    {titel: '', location: {lat: , lng: }},
-    {title: '', location: {lat: , lng: }},
-    {titel: '', location: {lat: , lng: }},
-    {title: '', location: {lat: , lng: }},
-    {titel: '', location: {lat: , lng: }}
+    {title: "StoutHaus Coffee Pub", location: {lat: 30.2325888, lng: -97.811333}},
+    {titel: "Torchy's Tacos", location: {lat: 30.2509257, lng: -97.7542336}},
+    {title: "Gordough's Big. Fat. Donuts.", location: {lat: 30.2495123, lng: -97.7548084}},
+    {titel: "Lick Honest Ice Creams", location: {lat: 30.2555995, lng: -97.7626348}},
+    {title: "St. Elmo Brewing Company", location: {lat: 30.2174538, lng: -97.7610629}}
   ]
 }
-*/
