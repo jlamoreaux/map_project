7a54433d0a60654ca7dbf23673046918adf5758d
Jorcan Lamoreaux <jnacious88@gmail.com>
2017-11-14 22:13:00 -0600

    cleaned up unneeded code

--- js/model.js
+++ js/model.js
@@ -3,34 +3,5 @@ function Location(newLocation) {
     return {
         title: ko.observable(newLocation.title),
         latLng: ko.observable(newLocation.latLng)
-    };
-};
-
-
-
-// https://codepen.io/KryptoniteDove/post/load-json-file-locally-using-pure-javascript
-/*function loadJSON(callback) {
-    $.ajax({
-        async: true,
-        url: locationsURL,
-        dataType: 'json',
-        error: alert('Unable to load locations'),
-        success: function(response) {
-            JSONLocations = response.data.locations;
-            for (i = 0; i < JSONLocations.length; i++) {
-                appViewModel.locations.push(new Location(JSONLocations[i]));
-                console.log(JSONLocations[i]);
-            }
-        }
-    });
-}*/
-
-/*function init() {
-    loadJSON();
-    //function(response) {
-    // Parse JSON string into object
-    //JSONLocations = JSON.parse(response.data);
-    //});
+    }
 }
-
-init();*/
